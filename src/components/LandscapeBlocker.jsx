b0e0f28dcee5d8f77205cebc05003198a6010847
rizwanNode <rizvi98ahmad@outlook.com>
2026-02-13 17:10:51 +0500

    final changes done

--- src/components/LandscapeBlocker.jsx
+++ src/components/LandscapeBlocker.jsx
@@ -2,7 +2,7 @@ const LandscapeBlocker = () => {
     return (
         <div className="landscape-blocker">
             <h1>PLEASE ROTATE DEVICE VERTICAL VIEW ONLY</h1>
-            <h2>MANIFESTED IN 404</h2>
+            <h2>MADE IN ATL ZONE IV</h2>
         </div>
     );
 };
